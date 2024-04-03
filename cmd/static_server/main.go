03bf31db2b5d3795525044cad1c02a43bee90d2d
confleux <confleux@gmail.com>
2024-04-03 14:32:56 +0300

    refactor: dir structure, port const

--- cmd/static_server/main.go
+++ cmd/static_server/main.go
@@ -7,19 +7,22 @@ import (
 	"os"
 )
 
+const (
+	PORT = "3000"
+)
+
 func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
-		port = "3000"
+		port = PORT
 	}
 
+	log.Printf("Starting server on port: %s...", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Fatalf("Unable to start server on port: %s", port)
 	}
-
-	log.Printf("Starting server on port: %s", port)
 }
