3f9a859eb57f0f30be0f67c69c05c912a9ff93f6
confleux <confleux@gmail.com>
2024-05-20 18:24:55 +0300

    chore: add user repository

---
+++ client-service/internal/repository/user.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
+)
+
+type UserRepository struct {
+	Pool *pgxpool.Pool
+}
+
+func NewUserRepository(pool *pgxpool.Pool) *UserRepository {
+	return &UserRepository{Pool: pool}
+}
+
+func (repo *UserRepository) CreateUser(ctx context.Context, uid string, email string) (string, error) {
+	createdAt := time.Now()
+
+	var res string
+
+	if err := repo.Pool.QueryRow(ctx, "INSERT INTO public.user (uid, email, created_at) VALUES ($1, $2, $3) RETURNING uid", uid, email, createdAt).Scan(&res); err != nil {
+		return "", fmt.Errorf("unable to create user: %w", err)
+	}
+
+	fmt.Println(res)
+
+	return res, nil
+}
